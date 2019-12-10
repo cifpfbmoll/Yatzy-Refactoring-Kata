5c9f3953a8c920f185edcd54bab7b828d839f989
DanyZurita <dzurita@cifpfbmoll.eu>
2019-12-10 19:32:30 +0100

    refactor(three_of_a_kind): Refactorizar lal funcion three_of_a_kind

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -21,14 +21,14 @@ def test_yatzy_scores_50():
     assert 0 == Yatzy.yatzy([6, 6, 6, 6, 3])
 
 
-def test_1s():
+def test_ones():
     assert Yatzy.ones(1, 2, 3, 4, 5) == 1
     assert 2 == Yatzy.ones(1, 2, 1, 4, 5)
     assert 0 == Yatzy.ones(6, 2, 2, 4, 5)
     assert 4 == Yatzy.ones(1, 2, 1, 1, 1)
 
 
-def test_2s():
+def test_twos():
     assert 4 == Yatzy.twos(1, 2, 3, 2, 6)
     assert 10 == Yatzy.twos(2, 2, 2, 2, 2)
 
@@ -38,7 +38,7 @@ def test_threes():
     assert 12 == Yatzy.threes(2, 3, 3, 3, 3)
 
 
-def test_fours_test():
+def test_fours():
     assert 12 == Yatzy.fours(4, 4, 4, 5, 5)
     assert 8 == Yatzy.fours(4, 4, 5, 5, 5)
     assert 4 == Yatzy.fours(4, 5, 5, 5, 5)
@@ -56,7 +56,7 @@ def test_sixes_test():
     assert 18 == Yatzy.sixes(6, 5, 6, 6, 5)
 
 
-def test_one_pair():
+def test_one_of_a_kind():
     assert 6 == Yatzy.score_pair(3, 4, 3, 5, 6)
     assert 10 == Yatzy.score_pair(5, 3, 3, 3, 5)
     assert 12 == Yatzy.score_pair(5, 3, 6, 6, 5)
@@ -64,7 +64,7 @@ def test_one_pair():
     assert 0 == Yatzy.score_pair(3, 4, 2, 5, 6)
 
 
-def test_two_Pair():
+def test_two_of_a_kind():
     assert 16 == Yatzy.two_pair(3, 3, 5, 4, 5)
     assert 18 == Yatzy.two_pair(3, 3, 6, 6, 6)
     assert 0 == Yatzy.two_pair(3, 3, 6, 5, 4)
@@ -77,7 +77,7 @@ def test_three_of_a_kind():
     assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 5)
 
 
-def test_four_of_a_knd():
+def test_four_of_a_kind():
     assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 5)
     assert 20 == Yatzy.four_of_a_kind(5, 5, 5, 4, 5)
     assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 3)
--- python/yatzy.py
+++ python/yatzy.py
@@ -116,31 +116,42 @@ class Yatzy:
 
     
     @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
-        return 0
+    def four_of_a_kind(*dices):
+        lista = []
+        QUARTET = 4
+        for dice in dices:
+            if dices.count(dice) >= QUARTET:
+                if lista.count(dice) == 0:
+                    lista.append(dice)
+                else:
+                    continue
+
+        if lista == []:
+            return 0
+        else:
+            lista.sort()
+            total = (lista[-1] * QUARTET)
+            return total
+
     
 
     @staticmethod
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i+1) * 3
-        return 0
+    def three_of_a_kind(*dices):
+        lista = []
+        TRIO = 3
+        for dice in dices:
+            if dices.count(dice) >= TRIO:
+                if lista.count(dice) == 0:
+                    lista.append(dice)
+                else:
+                    continue
+
+        if lista == []:
+            return 0
+        else:
+            lista.sort()
+            total = (lista[-1] * TRIO)
+            return total
     
 
     @staticmethod
